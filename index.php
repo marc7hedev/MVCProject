f1082967684743b14abcbc78f6e524d55a7993a2
marc7hedev <driberstagg@gmail.com>
2024-01-24 12:07:14 -0600

    Añadido framework Bulma

--- index.php
+++ index.php
@@ -18,6 +18,20 @@
     <?php require_once "./app/views/inc/head.php";?>
 </head>
 <body>
+
+    <div class="buttons">
+    <button class="button is-primary">Primary</button>
+    <button class="button is-link">Link</button>
+    </div>
+
+    <div class="buttons">
+    <button class="button is-info">Info</button>
+    <button class="button is-success">Success</button>
+    <button class="button is-warning">Warning</button>
+    <button class="button is-danger">Danger</button>
+    </div>
+
+
     <?php require_once "./app/views/inc/script.php";?>
     
 </body>
