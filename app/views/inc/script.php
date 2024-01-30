089c397be8f52cb936f3703aa3231704b81fd2a4
marc7hedev <driberstagg@gmail.com>
2024-01-29 20:15:56 -0600

    Añadidos archivos Js e implementado plugin de notificaciones SweetAlert

--- app/views/inc/script.php
+++ app/views/inc/script.php
@@ -1,3 +1,2 @@
-<?php
-    
-?>
+<script src="<?php echo APP_URL;?>app/views/js/ajax.js"></script>
+<script src="<?php echo APP_URL;?>app/views/js/main.js"></script>
--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 
-const APP_URL = "http://localhost/MVP/";
+const APP_URL = "http://localhost/MVC/";
 const APP_NAME = "CRUD POO MVC MYSQL";
 const APP_SESSION_NAME = "CRUD";
 date_default_timezone_set("America/Mexico_City");
