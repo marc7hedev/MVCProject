9afef38e417ce9c53ea1d125ccb3990957eb2a9d
marc7hedev <driberstagg@gmail.com>
2024-02-28 22:44:25 -0600

    Definiendo función para obtener vistas del controlador

--- app/controllers/viewsController.php
+++ app/controllers/viewsController.php
@@ -5,4 +5,13 @@ use app\models\viewsModel;
 
 class viewsController extends viewsModel{
 
+    public function obtenerVistasControlador($vista){
+        if($vista!=""){
+            $respuesta = $this->obtenerVistasModelo($vista);
+        }else{
+            $respuesta = "login";
+        }
+        return $respuesta;
+    }
+
 }
