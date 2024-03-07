5ea5602050718d3bb2f74226b7409ddb57ef44af
marc7hedev <driberstagg@gmail.com>
2024-03-07 12:45:50 -0600

    Añadida navBar y vista basica de dashboard

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
     <?php require_once "./app/views/inc/head.php";?>
 </head>
 <body>
-
-    <?php 
+    <?php
+    
     use app\controllers\viewsController;
     $viewsController = new viewsController();
     $vista=$viewsController->obtenerVistasControlador($url[0]);
@@ -27,10 +27,13 @@
     if($vista == "login" || $vista == "404"){
         require_once "./app/views/content/".$vista."-view.php";
     }else{
+        require_once "./app/views/inc/navbar.php";
         require_once $vista;
     }
 
     require_once "./app/views/inc/script.php";
+
+
     ?>
     
 </body>
