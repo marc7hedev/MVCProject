abdfa5c38c583cfccf65d486e5e6ce34989142b8
marc7hedev <driberstagg@gmail.com>
2024-03-13 09:45:38 -0600

    Creada conexión a base de datos con PDO

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -1,6 +1,7 @@
 <?php
     
     namespace app\models;
+    use \PDO;
 
     if(file_exists(__DIR__."/../../config/server.php")){
         require_once __DIR__."/../../config/server.php";
@@ -11,4 +12,19 @@
         private $db = DB_NAME;
         private $user = DB_USER;
         private $pass = DB_PASS;
+
+        protected function conectar(){
+            $conexion = new PDO(
+                "mysql:host=".$this->server.";
+                dbname=".$this->db,
+                $this->user,
+                $this->pass
+            );
+
+            $conexion->exec("SET CHARACTER SET utf8");
+            return $conexion;
+
+        }
+
+
     }
