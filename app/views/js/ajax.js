505e52acb55f829a89430b4f25f99304300aa8a8
marc7hedev <driberstagg@gmail.com>
2024-06-13 12:46:21 -0600

    Implementada funcion formularios_ajax para el envío de formularios

--- app/views/js/ajax.js
+++ app/views/js/ajax.js
@@ -14,9 +14,33 @@ formularios_ajax.forEach((formularios) => {
             cancelButtonText: "No, cancelar",
         }).then((result) => {
             if (result.isConfirmed) {
-                
+                let data = new FormData(this);
+                let method = this.getAttribute("method");
+                let action = this.getAttribute("action");
+
+                let encabezados = new Headers();
+
+                let config = {
+                    method: method,
+                    headers: encabezados,
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    body: data,
+                };
+
+                fetch(action, config)
+                    .then(respuesta => respuesta.json())
+                    .then(respuesta => {
+                        return alertas_ajax(respuesta);
+                    });
+
             }
         });
 
     });
 });
+
+
+function alertas_ajax(alerta){
+    
+};
