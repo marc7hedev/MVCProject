21f9f6cb1254f64550c0af8d4db3d6704d460a75
marc7hedev <driberstagg@gmail.com>
2024-07-11 17:38:22 -0600

    Implementando subir imagenes a servidor y validaciones en el userController

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -142,6 +142,83 @@
             # Directorio de imágenes #
             $img_dir = "../views/fotos/";
 
+            # Comprobar si se ha seleccionado una imagen #
+            if($_FILES['usuario_foto']['name'] != "" && 
+                $_FILES['usuario_foto']['size'] >0){
+                # Creando directorio #
+                if(!file_exists($img_dir)){
+                    if(!mkdir($img_dir, 0777)){
+                        $alerta = [
+                            "tipo"=>"simple",
+                            "titulo"=>"Ocurrió un error inesperado",
+                            "texto"=>"Error al crear el directorio",
+                            "icono"=>"error"
+                        ];
+                        return json_encode($alerta);
+                        exit();
+                    }
+                }
+
+                # Verificando formato de imágenes #
+                if(mime_content_type($_FILES['usuario_foto']['tmp_name']) != "image/jpeg" && mime_content_type($_FILES['usuario_foto']['tmp_name']) != "image/png"){
+                    $alerta = [
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrió un error inesperado",
+                        "texto"=>"La imagen que ha seleccionado es de un formato no permitido",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+
+                }
+
+                # Verificando peso de imagen #
+                if(($_FILES['usuario_foto']['size']/1024)>5120){
+                    $alerta = [
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrió un error inesperado",
+                        "texto"=>"La imagen que ha seleccionado excede el límite de 5 MB",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+                }
+
+                # Nombre de la foto #
+                $foto = str_ireplace(" ", "_", $nombre);
+                $foto = $foto."_".rand(0, 100);
+
+                # Extensión de la imagen #
+                switch(mime_content_type($_FILES['usuario_foto']['tmp_name'])){
+                    case "img/jpeg":
+                        $foto=$foto.".jpg";
+                    break;
+                    case "img/png":
+                        $foto=$foto.".png";
+                    break;
+                }
+
+                # Permisos de lectura y escritura #
+                chmod($img_dir, 0777);
+
+                # Moviendo imagen al directorio #
+                if (!move_uploaded_file($_FILES['usuario_foto']['tmp_name'], $img_dir.$foto)){
+                    $alerta = [
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrió un error inesperado",
+                        "texto"=>"No se puede subir la imagen al sistema en este momento",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+                }
+
+
+
+            } else {
+                $foto = "";
+            }
+
         
 
         }
