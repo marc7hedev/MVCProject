bb24f70cf4ebe6b62ab39b79371e0f590fde1c80
marc7hedev <driberstagg@gmail.com>
2024-07-18 21:10:39 -0600

    Implementando mostrar contenido de variables de sesión en sección Mi cuenta

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -40,14 +40,16 @@
         <div class="navbar-end">
             <div class="navbar-item has-dropdown is-hoverable">
                 <a class="navbar-link">
-                    ** User Name **
+                    <?php
+                        echo $_SESSION['usuario'];
+                    ?>
                 </a>
                 <div class="navbar-dropdown is-boxed">
 
-                    <a class="navbar-item" href="<?php echo APP_URL; ?>userUpdate/">
+                    <a class="navbar-item" href="<?php echo APP_URL."userUpdate/".$_SESSION['id']."/"; ?>">
                         Mi cuenta
                     </a>
-                    <a class="navbar-item" href="<?php echo APP_URL; ?>userPhoto/">
+                    <a class="navbar-item" href="<?php echo APP_URL."userPhoto/".$_SESSION['id']."/"; ?>">
                         Mi foto
                     </a>
                     <hr class="navbar-divider">
