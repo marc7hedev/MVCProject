fcd0b9bddc1d5cc8667abe20302059cb8fa9fd9e
marc7hedev <driberstagg@gmail.com>
2024-08-28 18:45:59 -0600

    Corregido mensaje de alerta en error_alert.php

--- app/views/inc/error_alert.php
+++ app/views/inc/error_alert.php
@@ -2,4 +2,7 @@
     <div class="message-header">
         <p>¡Ocurrió un error inesperado!</p>
     </div>
+    <div class="message-body">
+        No se pudieron cargar los datos solicitados
+    </div>
 </article>
