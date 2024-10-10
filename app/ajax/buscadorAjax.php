7fab3e50dcdc6cefdfdf4518462a42a90cdc1419
marc7hedev <driberstagg@gmail.com>
2024-10-10 13:17:42 -0600

    Implementado archivos base para searchController.php

---
+++ app/ajax/buscadorAjax.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "../../config/app.php";
+    require_once "../views/inc/session_start.php";
+    require_once "../../autoload.php";
+
+    use app\controllers\searchController;
+
+    if(isset($_POST['modulo_buscador'])){
+
+        $insBuscador = new searchController();
+
+        
+
+        
+
+    } else {
+        session_destroy();
+        header("Location: ".APP_URL."login/");
+    }
---
+++ app/controllers/searchController.php
@@ -0,0 +1,12 @@
+<?php
+    
+    namespace app\controllers;
+    use app\models\mainModel;
+
+    class searchController extends mainModel{
+
+
+        
+
+
+    }
